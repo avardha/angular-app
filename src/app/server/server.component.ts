2dd950c2ea7588657e55b14229588647025d107a
Akhila Vardha <sn.akhila@gmail.com>
2020-05-11 22:24:55 -0500

    initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  numberOfStocks = 10;
+  numberOfBonds  = 20;
+  maxLength = 2;
+  numberOfadjustStocks = 0;
+  numberOfadjustBonds = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  adjustStocks(event: Event){
+   this.numberOfadjustStocks =  Number((<HTMLInputElement> event.target).value);
+  }
+
+  adjustBonds(event: Event){
+    this.numberOfadjustBonds =  Number((<HTMLInputElement> event.target).value);
+  }
+
+  total(){
+    return (this.numberOfadjustStocks > 0 ? this.numberOfadjustStocks : this.numberOfStocks) +
+      (this.numberOfadjustBonds > 0 ? this.numberOfadjustBonds : this.numberOfBonds);
+  }
+
+}
